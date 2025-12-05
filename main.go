182f8746c33405201ee45d481499862cc49a2fcd
Raja Mahanty <rajamahanty2@gmail.com>
2025-12-05 22:44:50 +0530

    Add file creation and saving functionality to Totion

--- main.go
+++ main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/charmbracelet/bubbles/textarea"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 var (
+	vaultDir    string
 	cursorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("212"))
 )
+
+func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Error getting home directory", err)
+	}
+	vaultDir = fmt.Sprintf("%s/.totion", homeDir)
+}
 
 type model struct {
 	newFIleInput           textinput.Model
 	createFileInputVisible bool
+	currentFile            *os.File
+	noteTextArea           textarea.Model
 }
 
 func (m model) Init() tea.Cmd {
@@ -33,11 +46,68 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+n":
 			m.createFileInputVisible = true
 			return m, nil
+		case "ctrl+s":
+			// save
+			if m.currentFile == nil {
+				break
+			}
+
+			if err := m.currentFile.Truncate(0); err != nil {
+				fmt.Println("Cannot save the file :( ")
+				return m, nil
+			}
+
+			if _, err := m.currentFile.Seek(0, 0); err != nil {
+				fmt.Println("Cannot save the file :( ")
+				return m, nil
+			}
+
+			if _, err := m.currentFile.WriteString(m.noteTextArea.Value()); err != nil {
+				fmt.Println("Cannot save the file :( ")
+				return m, nil
+			}
+
+			if err := m.currentFile.Close(); err != nil {
+				fmt.Println("Cannot save the file :( ")
+				return m, nil
+			}
+
+			m.currentFile = nil
+			m.noteTextArea.SetValue("")
+
+			return m, nil
+		case "enter":
+			if m.currentFile != nil {
+				break
+			}
+			// todo: create file
+			fileName := m.newFIleInput.Value()
+			if fileName != "" {
+				filepath := fmt.Sprintf("%s/%s.md", vaultDir, fileName)
+
+				if _, err := os.Stat(filepath); err == nil {
+					return m, nil
+				}
+
+				f, err := os.Create(filepath)
+				if err != nil {
+					log.Fatalf("%v", err)
+				}
+
+				m.currentFile = f
+				m.createFileInputVisible = false
+				m.newFIleInput.SetValue("")
+			}
+			return m, nil
 		}
 	}
 
 	if m.createFileInputVisible {
 		m.newFIleInput, cmd = m.newFIleInput.Update(msg)
+	}
+
+	if m.currentFile != nil {
+		m.noteTextArea, cmd = m.noteTextArea.Update(msg)
 	}
 	return m, cmd
 }
@@ -58,17 +128,30 @@ func (m model) View() string {
 		MarginTop(1)
 
 	welcome := WelcomeStyle.Render("📒 Welcome to Totion")
+
+	help := HelpStyle.Render("Ctrl+N: new file | Ctrl+L: list files | Esc: back/save | Ctrl+S: save | Ctrl+C: quit")
+
 	view := ""
+
 	if m.createFileInputVisible {
 		view = m.newFIleInput.View()
 	}
-	help := HelpStyle.Render("Ctrl+N: new file | Ctrl+L: list files | Esc: back/save | Ctrl+S: save | Ctrl+C: quit")
+
+	if m.currentFile != nil {
+		view = m.noteTextArea.View()
+	}
 
 	return fmt.Sprintf("\n%s\n\n%s\n\n%s", welcome, view, help)
 }
 
 func initializeModel() model {
-	// initialize new files
+
+	err := os.MkdirAll(vaultDir, 0750)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// initialize new file input
 	ti := textinput.New()
 	ti.Placeholder = "What would you like to call it?"
 	ti.Focus()
@@ -78,9 +161,15 @@ func initializeModel() model {
 	ti.PromptStyle = cursorStyle
 	ti.TextStyle = cursorStyle
 
+	// textarea
+	ta := textarea.New()
+	ta.Placeholder = "Write your note here..."
+	ta.Focus()
+
 	return model{
 		newFIleInput:           ti,
 		createFileInputVisible: false,
+		noteTextArea:           ta,
 	}
 }
 
